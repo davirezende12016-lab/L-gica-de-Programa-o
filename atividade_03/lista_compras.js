02ec6aaea398b0834b3c8197d7ddcc17c5507364
calderacaio16-rgb <calderacaio16@gmail.com>
2026-03-12 12:21:29 -0300

    arrays

---
+++ atividade_03/lista_compras.js
@@ -0,0 +1,12 @@
+let lista_compras = ["Arroz","Feijão","Áçúcar","Salmão"];
+    lista_compras.push("Macarrão")
+lista_compras.push("Refrigerante")
+lista_compras. push("Desinfetante")
+lista_compras.forEach(Item => {
+        console.log(Item);
+    });
+    let cesta_frutas = ["Maça","Uva","Laranja","Banana"];
+    cesta_frutas = ["Pera", "Pitaya"];
+    cesta_frutas.forEach(fruta => {
+        console.log(fruta);
+    })
